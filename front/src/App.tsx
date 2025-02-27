58587b2f41bd2d91f7efab6715045a267f1cf78a
Bartosz Janik <bartekjanik45@gmail.com>
2025-02-27 20:43:56 +0100

    first changes on website

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,10 @@
 import "./App.css"
 
 function App() {
-  return <div className="text-red-500">nice</div>
+  return <div className="bg-neutral-800 text-neutral-100 h-screen w-screen">
+    <div className="text-center p-5"> Plany UBB
+    </div>
+  </div>
 }
 
 export default App
