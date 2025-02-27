0c03526f13c01f3723d387a40fc63ee58908e7c5
Jakub <kubajez02@gmail.com>
2025-02-28 00:00:03 +0100

    Wypisywanie hierarchiczne [Wydział->Katedra->Prowadzący]

---
+++ backend/main.py
@@ -0,0 +1,39 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+driver.get("https://plany.ubb.edu.pl/left_menu.php?type=2#")
+
+try:
+    # Pobierz wydziały
+    _wydzialy = WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul.main_tree.treeview > li"))
+    )
+
+    wydzialy = _wydzialy[1:-2]  # Pomijamy pierwszy i ostatnie dwa elementy
+
+    for wydzial in wydzialy:
+        print(wydzial.text)
+        strzalka = wydzial.find_element(By.CSS_SELECTOR, "img[src*='plus1.gif']")
+        driver.execute_script("arguments[0].click();", strzalka)
+        
+        katedry = WebDriverWait(wydzial, 10).until(
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul.treeview li"))
+        )
+        
+        for katedra in katedry:
+            print("\t" + katedra.text)
+            strzalka = katedra.find_element(By.CSS_SELECTOR, "img[src*='plus.gif']")
+            driver.execute_script("arguments[0].click();", strzalka)
+            
+            prowadzacy = WebDriverWait(katedra, 10).until(
+                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul li"))
+            )
+            
+            for osoba in prowadzacy:
+                print("\t\t" + osoba.text)
+
+finally:
+    driver.quit()
