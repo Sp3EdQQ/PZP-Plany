fc50ff7dafc4c7f236207e8cc24f1c6c6f870c1d
Jakub <kubajez02@gmail.com>
2025-02-28 00:00:41 +0100

    Kontrola logów

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,10 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from selenium import webdriver
+
+options = webdriver.ChromeOptions()
+options.add_argument("--log-level=3")  # 0 = INFO, 1 = WARNING, 2 = ERROR, 3 = FATAL (minimalne logi)
 driver = webdriver.Chrome()
 driver.get("https://plany.ubb.edu.pl/left_menu.php?type=2#")
 
@@ -16,18 +20,24 @@ try:
 
     for wydzial in wydzialy:
         print(wydzial.text)
+
+        # Wczytanie katedr
         strzalka = wydzial.find_element(By.CSS_SELECTOR, "img[src*='plus1.gif']")
         driver.execute_script("arguments[0].click();", strzalka)
         
+        # Pobierz katedry
         katedry = WebDriverWait(wydzial, 10).until(
             EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul.treeview li"))
         )
         
         for katedra in katedry:
             print("\t" + katedra.text)
+
+            # Wczytanie prowadzacych
             strzalka = katedra.find_element(By.CSS_SELECTOR, "img[src*='plus.gif']")
             driver.execute_script("arguments[0].click();", strzalka)
             
+            # Pobierz prowadzacych
             prowadzacy = WebDriverWait(katedra, 10).until(
                 EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ul li"))
             )
