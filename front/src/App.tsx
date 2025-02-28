b98345306facf5f3c75eeb79e952ba299c25f99d
Bartosz Janik <bartekjanik45@gmail.com>
2025-02-28 19:10:22 +0100

    refactor: downgraded to react-18 and added calendar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,21 @@
 import "./App.css"
+import { useState } from "react"
+import { Calendar } from "@/components/ui/calendar"
+
 
 function App() {
+
+  const [date, setDate] = useState<Date | undefined>(new Date())
+
   return <div className="bg-neutral-800 text-neutral-100 h-screen w-screen">
     <div className="text-center p-5"> Plany UBB
     </div>
+    <Calendar
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border"
+    />
   </div>
 }
 
