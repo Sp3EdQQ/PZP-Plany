7c050bee798036b14f67caea42f9e3a7ca707224
Bartosz Janik <bartekjanik45@gmail.com>
2025-02-28 21:04:37 +0100

    feat: current date under calendar

--- frontend/src/components/CalendarDiv.tsx
+++ frontend/src/components/CalendarDiv.tsx
@@ -3,15 +3,22 @@ import { Calendar } from "@/components/ui/calendar"
 
 export const CalendarDiv = () => {
   const [date, setDate] = useState<Date | undefined>(new Date())
-  const marginForCalendar: number = 5
+  const currentDate = new Date()
+  const currentDay = currentDate.getDay()
+  const currentMonth = String(currentDate.getMonth() + 1).padStart(2, "0")
+  const currentYear = String(currentDate.getFullYear())
+  console.log(date)
 
-  return (<div className={`w-1/5 ml-${marginForCalendar} mt-${marginForCalendar}`}>
+  return (<div className="w-1/5 flex-col m-6">
     <Calendar
       mode="single"
       selected={date}
       onSelect={setDate}
       className="rounded-md border w-64"
     />
+    <div className="pt-1">
+      Dzisiaj mamy {currentDay}.{currentMonth}.{currentYear}
+    </div>
   </div>)
 
 }
